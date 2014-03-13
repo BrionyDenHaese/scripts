012b18228eff2d29d1a4a436a908f4a95f0a5ace
BrionyDenHaese <brionydenhaese@hotmail.com>
2014-03-13 14:52:03 +0100

    Update keyboardlayout.sh

--- keyboardlayout.sh
+++ keyboardlayout.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-echo KEYTABLE="be-latin1">/etc/sysconfig/keyboard
-echo MODEL="pc105">>/etc/sysconfig/keyboard
-echo LAYOUT="nl">>/etc/sysconfig/keyboard
-echo KEYBOARDTYPE="pc">>/etc/sysconfig/keyboard
+echo 'KEYTABLE="be-latin1"'>/etc/sysconfig/keyboard
+echo 'MODEL="pc105"'>>/etc/sysconfig/keyboard
+echo 'LAYOUT="nl"'>>/etc/sysconfig/keyboard
+echo 'KEYBOARDTYPE="pc"'>>/etc/sysconfig/keyboard
 
 shutdown -r now
